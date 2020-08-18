3199d76d88cbd949ed831a491806d23655edc9bf
Anish shetty k <47324253+Anishshettyk@users.noreply.github.com>
2020-08-18 19:14:57 +0530

    version 1.0
    new feature added

--- js/main.js
+++ js/main.js
@@ -68,16 +68,89 @@ if (currentTheme) {
 })(jQuery);
 
 //weather report
-const weatherStat = document.getElementById('weather-stat');
-const placeInfoImg = document.getElementById('place-info-img');
 
-async function weatherReport() {
-    const apiurl = `http://api.weatherstack.com/current?access_key=e56d03fe39377be8dc40e4590cee8f99&query=puttur`;
-    const repsonse = await fetch(apiurl);
-    const data = await repsonse.json();
-    console.log(data);
+//restoring the data from html
+const cityName = document.getElementById('input-value');
+const btnSearch = document.getElementById('btn-search');
+const description = document.getElementById('ess-head');
+const temperatureValue = document.getElementById('ess-temp')
+const locationName = document.getElementById('ess-loc');
+const max = document.getElementById('max');
+const min = document.getElementById('min');
+const humidityValue = document.getElementById('humidity');
+const windSpeedValue = document.getElementById('wind-speed');
+const pressurevalue = document.getElementById('pressure');
+const changeImg = document.getElementById('ess-img');
 
-    weatherStat.innerText = data.current.weather_descriptions[0];
-    placeInfoImg.src = data.current.weather_icons[0];
+
+
+
+
+async function weatherDisplay() {
+    let name = cityName.value;
+    const apiKey = '6ff05d084236cc03549af40cb63a7b59';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}`;
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        const des = data.weather[0].description;
+        const kelvinTemp = data.main.temp;
+        const location = data.name;
+        const KelvinTempMax = data.main.temp_max;
+        const KelvinTempMin = data.main.temp_min;
+        const pressure = data.main.pressure;
+        const humidity = data.main.humidity;
+        const windSpeed = data.wind.speed;
+
+
+        const temperature = Math.floor(parseInt(kelvinTemp) - 273.15);
+        const temperatureMax = Math.ceil(parseInt(KelvinTempMax) - 273.15);
+        const temperatureMin = Math.floor(parseInt(KelvinTempMin) - 273.15);
+
+
+        description.innerText = des;
+        locationName.innerText = location;
+        temperatureValue.innerHTML = `${temperature} <span>&#176;</span> C`;
+        max.innerHTML = `${temperatureMax} <span>&#176;</span> C`;
+        min.innerHTML = `${temperatureMin} <span>&#176;</span> C`;
+        humidityValue.innerText = `${humidity} %`;
+        windSpeedValue.innerText = `${windSpeed} kph`;
+        pressurevalue.innerText = `${pressure} pa`;
+
+        //photo change with respect to weather
+        if (des === "overcast clouds") {
+            changeImg.setAttribute('src', '/images/icons/overcast.png')
+        } else if (des === 'clear sky') {
+            changeImg.setAttribute('src', '/images/icons/clear sky.png')
+        } else if (des == 'haze') {
+            changeImg.setAttribute('src', '/images/icons/haze.png');
+        } else if (des == 'scattered clouds') {
+            changeImg.setAttribute('src', '/images/icons/scattered.png');
+        } else if (des == 'light rain') {
+            changeImg.setAttribute('src', '/images/icons/light.png');
+        }
+
+    } catch (error) {
+        console.log(error);
+        alert('ENTER A VALID CITY NAME');
+    }
 }
-weatherReport();
+
+
+
+btnSearch.addEventListener('click', weatherDisplay);
+document.addEventListener('keypress', (event) => {
+    if (event.keyCode === 13) {
+        weatherDisplay();
+    }
+});
+
+//add todays date
+const todayDate = document.getElementById('ess-date');
+const date = new Date();
+const formatDate = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+};
+todayDate.textContent = date.toLocaleDateString('en-US', formatDate);
